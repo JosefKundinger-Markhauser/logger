533c68305d1baed1e87c54e9952b16b4e75f5910
Josef Kundinger-Markhauser <joeymarkhauser@yahoo.ca>
2023-07-17 20:12:10 +0000

    minor update

--- Logger/Logger.py
+++ Logger/Logger.py
@@ -1,6 +1,25 @@
 import os
 from datetime import datetime
 
+"""
+%a: Returns the first three characters of the weekday, e.g. Wed.
+%A: Returns the full name of the weekday, e.g. Wednesday.
+%B: Returns the full name of the month, e.g. September.
+%w: Returns the weekday as a number, from 0 to 6, with Sunday being 0.
+%m: Returns the month as a number, from 01 to 12.
+%p: Returns AM/PM for time.
+%y: Returns the year in two-digit format, that is, without the century. For example, "18" instead of "2018".
+%f: Returns microsecond from 000000 to 999999.
+%Z: Returns the timezone.
+%z: Returns UTC offset.
+%j: Returns the number of the day in the year, from 001 to 366.
+%W: Returns the week number of the year, from 00 to 53, with Monday being counted as the first day of the week.
+%U: Returns the week number of the year, from 00 to 53, with Sunday counted as the first day of each week.
+%c: Returns the local date and time version.
+%x: Returns the local version of date.
+%X: Returns the local version of time.
+"""
+
 
 class TextFormat:
     HEADER = '\033[95m'
@@ -41,7 +60,7 @@ class Logger:
             return
         if self._log_file_name is None:
             self._log_file_name = self.get_timestamp().replace(":", "_")\
-                .replace(" ", "_")
+                .replace(" ", "_").replace("/", "_")
         full_path = os.path.join(self._log_dir, self._log_file_name)
         try:
             with open(full_path, "a") as f:
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 
 from Logger.Logger import Logger
 
-logger = Logger(debug=True)
+logger = Logger(debug=True, timestamp_format="%X %x")
 
 logger.log_header("Test Header\nHeader second line")
 logger.log("Test Log")
